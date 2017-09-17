225482b3310320a89ee7e6db70a037a6797622c3
Tortera1999 <nikhilnarendra1999@gmail.com>
2017-09-17 11:55:49 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,36 @@
-chrome.browserAction.onClicked.addListener(checkIfBgRun);
+console.log("I am background.js");
 
-function checkIfBgRun {
-	var popups = chrome.extension.getViews({type: "popup"});
-	if (0 < popups.length)
-		popups[0].variable = 42;
+var curURL;
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.greeting == "hello"){
+		chrome.tabs.query({
+						'active': true, 
+						'windowId': chrome.windows.WINDOW_ID_CURRENT
+						}, function(tabs){
+			sendResponse({
+				msg : extractHostname(tabs[0].url)
+			})			
+		});
+	}
+	return true;
+  });
+ 
+function extractHostname(url) {
+    var hostname;
+
+
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    hostname = hostname.split(':')[0];
+
+    hostname = hostname.split('?')[0];
+
+    return hostname;
 }
